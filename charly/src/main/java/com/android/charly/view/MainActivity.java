92e7e09b13fd2422b7e3d3ee1a9b084d1f4e1803
jdandrade <joao.andrade@aptoide.com>
2017-05-09 21:49:14 +0100

    Fixed Crash while opening CharlyActivity

--- charly/src/main/java/com/android/charly/view/MainActivity.java
+++ charly/src/main/java/com/android/charly/view/MainActivity.java
@@ -1,20 +1,21 @@
 package com.android.charly.view;
 
-import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import com.android.charly.R;
+import com.android.charly.data.HttpRequest;
+import com.android.charly.view.requests.OnListItemClickCallback;
 import com.android.charly.view.requests.RequestListFragment;
 
-public class MainActivity extends CharlyActivity {
+public class MainActivity extends CharlyActivity implements OnListItemClickCallback {
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.charly_activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
-    toolbar.setSubtitle(getApplicationName());
+    toolbar.setSubtitle(R.string.app_name);
     if (savedInstanceState == null) {
       getSupportFragmentManager().beginTransaction()
           .add(R.id.container, RequestListFragment.newInstance())
@@ -22,9 +23,7 @@ public class MainActivity extends CharlyActivity {
     }
   }
 
-  private String getApplicationName() {
-    ApplicationInfo applicationInfo = getApplicationInfo();
-    int stringId = applicationInfo.labelRes;
-    return stringId == 0 ? applicationInfo.nonLocalizedLabel.toString() : getString(stringId);
+  @Override public void onListFragmentInteraction(HttpRequest httpRequest) {
+
   }
 }
